2d0d5d94193d5a65cdac700d811e9d75e849a7c9
Sebastian Thiel <byronimo@gmail.com>
2016-12-25 12:57:43 +0100

    fix(travis): enforce latest cmake

--- tests/index.rs
+++ tests/index.rs
@@ -7,5 +7,6 @@ use tempdir::TempDir;
 #[test]
 fn clone_if_needed() {
     let tmp: TempDir = TempDir::new("new-index").unwrap();
-    let index = Index::at_path(tmp.path()).expect("successful clone to be created");
+    Index::at_path(tmp.path()).expect("successful clone to be created");
+    Index::at_path(tmp.path()).expect("second instance re-uses existing clone");
 }

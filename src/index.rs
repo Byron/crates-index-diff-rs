f9d531a63269e8e236489c9a7bb56a6bafdd9eeb
Sebastian Thiel <byronimo@gmail.com>
2016-12-25 12:50:35 +0100

    imp(travis): simplify travis
    travis-cargo does nothing for us in this case.

--- src/index.rs
+++ src/index.rs
@@ -14,11 +14,12 @@ impl Index {
     pub fn at_path<P>(path: P) -> Result<Index, GitError>
         where P: AsRef<Path>
     {
-        let repo = Repository::open(path.as_ref()).or_else(|err: GitError| if err.class() == ErrorClass::Repository {
-            RepoBuilder::new().bare(true).clone(INDEX_GIT_URL, path.as_ref())
-        } else {
-            Err(err)
-        })?;
+        let repo =
+            Repository::open(path.as_ref()).or_else(|err| if err.class() == ErrorClass::Repository {
+                    RepoBuilder::new().bare(true).clone(INDEX_GIT_URL, path.as_ref())
+                } else {
+                    Err(err)
+                })?;
 
         Ok(Index { repo: repo })
     }

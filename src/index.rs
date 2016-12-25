d49f62fa41dbba9278ec2080ae2b91f72dc6834e
Sebastian Thiel <byronimo@gmail.com>
2016-12-25 14:54:35 +0100

    imp(index): first traversal method
    The test fails for the wrong reason though, as it fails to
    parse my refspec even though libgit2 seems to be able to do it
    properly, and a recent-enough version is used.

--- src/index.rs
+++ src/index.rs
@@ -1,13 +1,21 @@
 use std::path::Path;
 
-use git2::Repository;
 use git2::build::RepoBuilder;
-use git2::{ErrorClass, Error as GitError};
+use git2::{Tree, Repository, Oid, ErrorClass, Error as GitError};
 
 static INDEX_GIT_URL: &'static str = "https://github.com/rust-lang/crates.io-index";
 
 pub struct Index {
     repo: Repository,
+}
+
+pub enum ChangeType {
+    Added
+}
+
+pub struct Crate {
+    pub name: String,
+    pub state: ChangeType
 }
 
 impl Index {
@@ -22,5 +30,17 @@ impl Index {
                 })?;
 
         Ok(Index { repo: repo })
+    }
+
+    pub fn traverse_changes<S1, S2>(&self, from: S1, to: S2) -> Result<Vec<Crate>, GitError>
+        where S1: AsRef<str>,
+              S2: AsRef<str>
+    {
+        fn into_tree<S: AsRef<str>>(repo: &Repository, rev: S) -> Result<Tree, GitError> {
+            repo.revparse_single(rev.as_ref()).and_then(|obj| repo.find_tree(obj.id()))
+        }
+
+        let (from, to) = (into_tree(&self.repo, from)?, into_tree(&self.repo, to)?);
+        Ok(Vec::new())
     }
 }
--- tests/index.rs
+++ tests/index.rs
@@ -3,10 +3,30 @@ extern crate tempdir;
 
 use crates_index_diff::*;
 use tempdir::TempDir;
+use std::env;
+use std::path::PathBuf;
+
+const rev_one_added: &'static str = "6a65b384404d244cf3fa8571131a0187d8d5abc6";
 
 #[test]
 fn clone_if_needed() {
-    let tmp: TempDir = TempDir::new("new-index").unwrap();
+    let tmp = TempDir::new("new-index").unwrap();
     Index::at_path(tmp.path()).expect("successful clone to be created");
     Index::at_path(tmp.path()).expect("second instance re-uses existing clone");
+}
+
+#[test]
+fn traverse_changed_crates() {
+    let tmp = TempDir::new("new-index").unwrap();
+    let index = Index::at_path(env::var("CRATES_INDEX_DIFF_TEST_EXISTING_INDEX")
+            .map(PathBuf::from)
+            .unwrap_or(tmp.path().to_owned()))
+        .expect("successful clone");
+
+    assert_eq!(index.traverse_changes("foo", rev_one_added).is_err(), true);
+    assert_eq!(index.traverse_changes(rev_one_added, "bar").is_err(), true);
+
+    let crates: Vec<Crate> = index.traverse_changes(format!("{}~1", rev_one_added), rev_one_added)
+        .expect("ids to be valid and diff ok");
+    assert_eq!(crates.len(), 1);
 }

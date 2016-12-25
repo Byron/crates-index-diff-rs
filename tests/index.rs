f89651be89638e8210b74dc8843d5985d28e842b
Sebastian Thiel <byronimo@gmail.com>
2016-12-25 15:01:00 +0100

    fix(test): finally the correct refspec
    Fair enough, need trees here after all!

--- tests/index.rs
+++ tests/index.rs
@@ -6,7 +6,7 @@ use tempdir::TempDir;
 use std::env;
 use std::path::PathBuf;
 
-const rev_one_added: &'static str = "6a65b384404d244cf3fa8571131a0187d8d5abc6";
+const rev_one_added: &'static str = "615c9c41942a3ba13e088fbcb1470c61b169a187";
 
 #[test]
 fn clone_if_needed() {
@@ -26,7 +26,7 @@ fn traverse_changed_crates() {
     assert_eq!(index.traverse_changes("foo", rev_one_added).is_err(), true);
     assert_eq!(index.traverse_changes(rev_one_added, "bar").is_err(), true);
 
-    let crates: Vec<Crate> = index.traverse_changes(format!("{}~1", rev_one_added), rev_one_added)
+    let crates: Vec<Crate> = index.traverse_changes(format!("{}~1^{{tree}}", rev_one_added), format!("{}^{{tree}}", rev_one_added))
         .expect("ids to be valid and diff ok");
     assert_eq!(crates.len(), 1);
 }

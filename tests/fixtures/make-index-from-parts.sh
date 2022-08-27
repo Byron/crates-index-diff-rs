d28591be86fb10b00f4db4f07cb1399c3b4305de
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-08-27 14:43:26 +0800

    build git repository from parts (#16)

--- tests/fixtures/make-index-from-parts.sh
+++ tests/fixtures/make-index-from-parts.sh
@@ -2,15 +2,17 @@
 
 set -eu -o pipefail
 
-parts="tests/fixtures/index-parts"
+parts="${1:?first argument is the repoitory root}/tests/fixtures/index-parts"
 commit_list=$parts/commit.list
 
 git init
-tar -x $parts/init.*.tar
+tar -x < $parts/init.*.tar
 git add . && git commit -m "initial commit"
 
 
 while read -r commit; do
     patch -p1 < "$parts/$commit.diff"
+    git add .
     git commit -F "$parts/$commit.msg"
 done < "$commit_list"
+git gc
--- tests/index/mod.rs
+++ tests/index/mod.rs
@@ -5,13 +5,18 @@ mod changes_from_objects {
     use crate::index::index_ro;
 
     #[test]
-    #[ignore]
     fn addition() {
         let _index = index_ro();
     }
 }
 
 fn index_ro() -> Index {
-    let dir = git_testtools::scripted_fixture_repo_read_only("make-index-from-parts.sh").unwrap();
+    let dir = git_testtools::scripted_fixture_repo_read_only_with_args(
+        "make-index-from-parts.sh",
+        std::env::current_dir()
+            .ok()
+            .map(|p| p.to_str().unwrap().to_owned()),
+    )
+    .unwrap();
     Index::from_path_or_cloned(dir).unwrap()
 }

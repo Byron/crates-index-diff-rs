0790daac499dcc50a512b57885d2a66fbc32bb59
Denis Cornehl <denis@cornehl.org>
2025-11-02 22:04:23 +0100

    fix!: Box<> big error variants for clippy and performance reasons

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -4,6 +4,28 @@ use std::str;
 static INDEX_GIT_URL: &str = "https://github.com/rust-lang/crates.io-index";
 static LAST_SEEN_REFNAME: &str = "refs/heads/crates-index-diff_last-seen";
 
+/// Declarative macro to generate `impl From<Src> for Error` where the source
+/// error value is boxed into the given `Error` enum variant.
+///
+/// Usage:
+/// impl_from_boxed!(gix::object::find::existing::Error => ErrorEnum::FindObject);
+/// impl_from_boxed!(gix::remote::find::existing::Error => ErrorEnum::FindRemote);
+///
+/// See also:
+/// * https://rust-lang.github.io/rust-clippy/stable/index.html#/result_large_err
+/// * https://github.com/dtolnay/thiserror/pull/419
+/// * https://github.com/dtolnay/thiserror/pull/418
+/// * https://github.com/dtolnay/thiserror/issues/302
+macro_rules! impl_from_boxed {
+    ($src:path => $err:ident::$variant:ident) => {
+        impl From<$src> for $err {
+            fn from(value: $src) -> Self {
+                Self::$variant(Box::new(value))
+            }
+        }
+    };
+}
+
 /// Options for cloning the crates-io index.
 pub struct CloneOptions {
     /// The url to clone the crates-index repository from.
